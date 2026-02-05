88233528e50720898d5f736b6a0b0a51987e214a
Fabrício Leon <fabricio.leonn@gmail.com>
2026-02-05 13:38:35 -0300

    fix: reset next config for vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  // Disable strict linting/type checking on build to unblock deployment
   eslint: {
     ignoreDuringBuilds: true,
   },
